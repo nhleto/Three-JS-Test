f93adcb7bd4b43b0d81047eb7084cf05268164bc
Henry <hnr.leto11@gmail.com>
2021-07-05 11:11:09 -0400

    add stars + offset camera

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import './style.css'
-
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
@@ -14,7 +14,7 @@ const renderer = new THREE.WebGL1Renderer({
 renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(window.innerWidth, window.innerHeight)
 
-camera.position.setZ(30)
+camera.position.set(0, 30, 40)
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
 const material = new THREE.MeshStandardMaterial({color: 0xFF6347})
@@ -24,16 +24,30 @@ const pointlight = new THREE.PointLight(0xffffff)
 pointlight.position.set(10, -10, 5)
 const ambientlight = new THREE.AmbientLight(0xffffff)
 const helper = new THREE.PointLightHelper(pointlight)
+const gridHelper = new THREE.GridHelper(200, 50)
+const controls = new OrbitControls(camera, renderer.domElement)
 
-scene.add(pointlight, ambientlight, helper)
-scene.add(torus)
+scene.add(torus, pointlight, ambientlight, helper, gridHelper)
+
+function addStar(){
+  const geometry = new THREE.SphereGeometry(.25, 24, 24)
+  const material = new THREE.MeshStandardMaterial({color: 0xffffff})
+  const star = new THREE.Mesh(geometry, material)
+
+  // @ts-ignore
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+  star.position.set(x, y, z)
+  scene.add(star)
+}
+// @ts-ignore
+Array(200).fill().forEach(addStar);
 
 function animate(){
   requestAnimationFrame(animate)
   torus.rotation.x += 0.01
   torus.rotation.y += 0.005
   torus.rotation.z += 0.01
-
+  controls.update()
   renderer.render(scene, camera)
 }
 animate()
