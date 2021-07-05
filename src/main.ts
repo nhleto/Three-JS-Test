93a7a1b3fc51f9a7d688ad3b590e0eb29c179ad4
Henry <hnr.leto11@gmail.com>
2021-07-05 10:57:42 -0400

    add light + helpers

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,37 @@ import './style.css'
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75,
+    window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGL1Renderer({
+// @ts-ignore
   canvas: document.querySelector(".bg")
 })
 
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+camera.position.setZ(30)
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
+const material = new THREE.MeshStandardMaterial({color: 0xFF6347})
+const torus = new THREE.Mesh(geometry, material)
+
+const pointlight = new THREE.PointLight(0xffffff)
+pointlight.position.set(10, -10, 5)
+const ambientlight = new THREE.AmbientLight(0xffffff)
+const helper = new THREE.PointLightHelper(pointlight)
+
+scene.add(pointlight, ambientlight, helper)
+scene.add(torus)
+
+function animate(){
+  requestAnimationFrame(animate)
+  torus.rotation.x += 0.01
+  torus.rotation.y += 0.005
+  torus.rotation.z += 0.01
+
+  renderer.render(scene, camera)
+}
+animate()
